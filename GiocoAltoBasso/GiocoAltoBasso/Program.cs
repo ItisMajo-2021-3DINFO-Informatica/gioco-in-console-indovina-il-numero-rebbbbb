19c63e20f88eefdc8d51eee321b481b668f48d8d
rebb <98151576+rebbbbb@users.noreply.github.com>
2022-02-18 10:42:38 +0100

    GiocoAltoBasso

---
+++ GiocoAltoBasso/GiocoAltoBasso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GiocoAltoBasso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Se vuoi finire il gioco, digitare 0");
+
+            //generatore di numeri casuali tra 1 e 100
+
+            while (numero != )
+            {
+                Console.WriteLine("Inserisci un numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero > 100)
+                {
+                    Console.WriteLine("Il numero inserito è troppo alto");
+                }
+                else if (numero < 1)
+                {
+                    Console.WriteLine("Il numero inserito è troppo basso");
+                }
+                else if (numero == 0)
+                {
+                    Console.WriteLine("Hai finito il gioco");
+                }
+            }
+        }
+    }
+}
